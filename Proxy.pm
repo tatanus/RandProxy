40b52278ad49c8107c19e7f1fdc59a0776e7d85e
Adam Compton <adam.compton@gmail.com>
2014-12-07 01:16:38 -0500

    iover due update

--- Proxy.pm
+++ Proxy.pm
@@ -38,6 +38,7 @@ package Proxy;
         	print "-- loaded [".@proxies."] proxies\n";
 
 		removeBadProxies();
+		checkProxies();
 		writeGoodProxiesFile();
 
         	print "-- there are [".@proxies."] proxies\n";
@@ -52,74 +53,167 @@ package Proxy;
 
 		### ##########################################################################
 		###
+		### Load Proxies from [ http://multiproxy.org/txt_anon/proxy.txt ]
+		###
+		### ##########################################################################
+		$ua = new LWP::UserAgent;
+		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
+
+		$count = $#proxies;
+
+        	print "-- Reading proxies from [http://multiproxy.org/txt_anon/proxy.txt]\n";
+		my $req = new HTTP::Request GET => 'http://multiproxy.org/txt_anon/proxy.txt';
+		my $res = $ua->request($req);
+		my $proxylist= $res->content;
+
+		if (defined $proxylist){
+			@data = $proxylist;
+			foreach $line (@data){
+				while ($line =~ m/([\d\.]{7,}:\d{1,})/ig){
+					push(@proxies, 'http://'.$1);
+				}
+			}
+			@proxies = uniq(@proxies);
+			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
+		} else {
+        		print "-- No proxies read from [http://proxy-list.org/english/index.php]\n";
+		}
+
+		### ##########################################################################
+		###
+		### Load Proxies from [ http://proxy-list.org/english/index.php ]
+		###
+		### ##########################################################################
+		$ua = new LWP::UserAgent;
+		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
+
+		$count = $#proxies;
+
+        	print "-- Reading proxies from [http://proxy-list.org/english/index.php]\n";
+		my $req = new HTTP::Request GET => 'http://proxy-list.org/english/index.php';
+		my $res = $ua->request($req);
+		my $proxylist= $res->content;
+
+		if (defined $proxylist){
+			@data = $proxylist;
+			foreach $line (@data){
+				while ($line =~ m/class="proxy">([\d\.]{7,}:\d{1,})<\/li>/ig){
+					push(@proxies, 'http://'.$1);
+				}
+			}
+			@proxies = uniq(@proxies);
+			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
+		} else {
+        		print "-- No proxies read from [http://proxy-list.org/english/index.php]\n";
+		}
+
+		### ##########################################################################
+		###
+		### Load Proxies from [ http://www.atomintersoft.com/anonymous_proxy_list ]
+		###
+		### ##########################################################################
+		$ua = new LWP::UserAgent;
+		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
+
+		$count = $#proxies;
+
+        	print "-- Reading proxies from [http://www.atomintersoft.com/anonymous_proxy_list]\n";
+		my $req = new HTTP::Request GET => 'http://www.atomintersoft.com/anonymous_proxy_list';
+		my $res = $ua->request($req);
+		my $proxylist= $res->content;
+
+		if (defined $proxylist){
+			@data = $proxylist;
+			foreach $line (@data){
+				if ($line =~ /Check proxy /i){
+					while ($line =~ m/Check proxy ([\d\.]{7,}:\d{1,}) now/ig){
+						push(@proxies, 'http://'.$1);
+					}
+				} else {
+					print "Fail\n";
+				}
+			}
+			@proxies = uniq(@proxies);
+			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
+		} else {
+        		print "-- No proxies read from [http://www.malwaregroup.com/Proxies]\n";
+		}
+
+		### ##########################################################################
+		###
 		### Load Proxies from [ http://www.malwaregroup.com/Proxies ]
 		###
 		### ##########################################################################
-#		$ua = new LWP::UserAgent;
-#		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
-#
-#		$count = $#proxies;
-#
-#        	print "-- Reading proxies from [http://www.malwaregroup.com/Proxies]\n";
-#		my $req = new HTTP::Request GET => 'http://www.malwaregroup.com/Proxies';
-#		my $res = $ua->request($req);
-#		my $proxylist= $res->content;
-#
-#		if (defined $proxylist){
-#			@data = $proxylist;
-#			foreach $line (@data){
-#				if ($line =~ /\">([\d\.]{7,}.*?)\<\/a\>\<\/td\>\s\<td\>(.*?)\<\/td\>/i){
-#					while ($line =~ m/\">([\d\.]{7,}.*?)\<\/a\>\<\/td\>\s\<td\>(.*?)\<\/td\>/ig){
-#						push(@proxies, 'http://'.$1.':'.$2);
-#					}
-#				} else {
-#					print "Fail\n";
-#				}
-#			}
-#			@proxies = uniq(@proxies);
-#			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
-#		} else {
-#        		print "-- No proxies read from [http://www.malwaregroup.com/Proxies]\n";
-#		}
-#
-#		### ##########################################################################
-#		###
-#		### Load Proxies from [ http://www.freeproxylists.com/anonymous.html ]
-#		###
-#		### ##########################################################################
-#		$ua = new LWP::UserAgent;
-#		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
-#
-#		$count = scalar $#proxies;
-#
-#		print "-- Reading proxies from [http://www.freeproxylists.com/anonymous.html]\n";
-#		my $req = new HTTP::Request GET => 'http://www.freeproxylists.com/anonymous.html';
-#		my $res = $ua->request($req);
-#		my $proxylist= $res->content;
-#
-#		if (defined $proxylist){
-#			@data = $proxylist;
-#			foreach $line (@data){
-#				while ($line =~ /anon\/(\d+)\.html/g) {
-#					my $req2 = new HTTP::Request GET => 'http://www.freeproxylists.com/load_anon_'.$1.'.html';
-#					my $res2 = $ua->request($req2);
-#					my $proxylist2 = $res2->content;
-#		
-#					if (defined $proxylist2){
-#						@data2 = $proxylist2;
-#						foreach $line2 (@data2){
-#							while ($line2 =~ /gt;(\d+\.\d+\.\d+\.\d+)&lt;\/td&gt;&lt;td&gt;(\d+)&lt/g) {
-#								push(@proxies, 'http://'.$1.':'.$2);
-#							}
-#						}
-#					}
-#				}
-#			}
-#			@proxies = uniq(@proxies);
-#			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
-#		} else {
-#        		print "-- No proxies read from [http://www.freeproxylists.com/anonymous.html]\n";
-#		}
+		$ua = new LWP::UserAgent;
+		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
+
+		$count = $#proxies;
+
+        	print "-- Reading proxies from [http://www.malwaregroup.com/Proxies]\n";
+		my $req = new HTTP::Request GET => 'http://www.malwaregroup.com/Proxies';
+		my $res = $ua->request($req);
+		my $proxylist= $res->content;
+
+		if (defined $proxylist){
+			@data = $proxylist;
+			foreach $line (@data){
+				if ($line =~ /\">([\d\.]{7,}.*?)\<\/a\>\<\/td\>\s\<td\>(.*?)\<\/td\>/i){
+					while ($line =~ m/\">([\d\.]{7,}.*?)\<\/a\>\<\/td\>\s\<td\>(.*?)\<\/td\>/ig){
+						push(@proxies, 'http://'.$1.':'.$2);
+					}
+				} else {
+					print "Fail\n";
+				}
+			}
+			@proxies = uniq(@proxies);
+			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
+		} else {
+        		print "-- No proxies read from [http://www.malwaregroup.com/Proxies]\n";
+		}
+
+		### ##########################################################################
+		###
+		### Load Proxies from [ http://www.freeproxylists.com/anonymous.html ]
+		###
+		### ##########################################################################
+		$ua = new LWP::UserAgent;
+		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
+
+		$count = scalar $#proxies;
+
+		print "-- Reading proxies from [http://www.freeproxylists.com/anonymous.html]\n";
+		my $req = new HTTP::Request GET => 'http://www.freeproxylists.com/anonymous.html';
+		my $res = $ua->request($req);
+		my $proxylist= $res->content;
+
+		if (defined $proxylist){
+			@data = $proxylist;
+			foreach $line (@data){
+				while ($line =~ /anon\/(\d+)\.html/g) {
+					my $req2 = new HTTP::Request GET => 'http://www.freeproxylists.com/load_anon_'.$1.'.html';
+					my $res2 = $ua->request($req2);
+					my $proxylist2 = $res2->content;
+		
+					if (defined $proxylist2){
+						@data2 = $proxylist2;
+						foreach $line2 (@data2){
+							while ($line2 =~ /gt;(\d+\.\d+\.\d+\.\d+)&lt;\/td&gt;&lt;td&gt;(\d+)&lt/g) {
+								push(@proxies, 'http://'.$1.':'.$2);
+							}
+						}
+					}
+				}
+			}
+			@proxies = uniq(@proxies);
+			print "-- Loaded [".($#proxies - $count)."] proxies.\n";
+		} else {
+        		print "-- No proxies read from [http://www.freeproxylists.com/anonymous.html]\n";
+		}
 
 		### ##########################################################################
 		###
@@ -128,6 +222,7 @@ package Proxy;
 		### ##########################################################################
 		$ua = new LWP::UserAgent;
 		$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
 
 		$count = $#proxies;
 
@@ -180,7 +275,7 @@ package Proxy;
 		while (<FILE>) {
 			chomp;
 			$tempProxy = $_;
-        		print "--- removing proxy [".$tempProxy."]\n";
+			#print "--- removing proxy [".$tempProxy."]\n";
         		@proxies = grep {$_ ne $tempProxy} @proxies;
 		}
 		close FILE;
@@ -199,12 +294,27 @@ package Proxy;
 
         	print "--- Removed! [".@proxies."] remaining\n";
 
-		if (@proxies <= 5) {
-			#	getProxies();
+		checkProxies();
+	}
+
+	sub checkProxies {
+                if (@proxies <= 5) {
+                	loadProxiesFromWeb();
+	                @proxies = uniq(@proxies);
+
+	                print "-- loaded [".@proxies."] proxies\n";
+
+                	removeBadProxies();
+        	        writeGoodProxiesFile();
+		}
+		if (@proxies == 0) {
+			print "ERROR:   Ran out of Proxies and could not get anymore!\n";
+			exit(0);
 		}
 	}
 
 	sub selectRandomProxy {
+		checkProxies();
         	my $randomIdx = rand($#proxies);
 
         	print "-- using proxy [". $proxies[$randomIdx] ."]\n";
@@ -215,10 +325,15 @@ package Proxy;
 	sub getURL {
 		my $self = shift;
 		my $url = shift;
+		my $count = shift;
 		
+		if ($count == 10) {
+			print "-- ERROR: Too many proxy errors!\n";
+			return;
+		}
+
 		my $ua = LWP::UserAgent->new;
         	print "- getting URL [".$url."]\n";
-        	$ua->timeout(10);
 
         	if (@proxies) {
         	        $tempProxy = selectRandomProxy();
@@ -228,11 +343,12 @@ package Proxy;
         	}
 		
         	$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
+		$ua->timeout(30);
         	my $response = $ua->get($url);
 
 		if ($response->status_line !~ /200/) { 
 	       	        removeProxy($tempProxy);
-			getURL($self,$url);
+			getURL($self,$url,$count+1);
 			return;
         	}
 		print "$tempProxy -- STATUSLINE = [".$response->status_line."]";
@@ -248,6 +364,12 @@ package Proxy;
 		my $self = shift;
 		my $url = shift;
 		my $file = shift;
+		my $count = shift;
+
+		if ($count == 10) {
+			print "-- ERROR: Too many proxy errors!\n";
+			return;
+		}
 
 		return(0) unless defined($file);
 
@@ -255,7 +377,7 @@ package Proxy;
 
 		my $ua = new LWP::UserAgent;
         	$ua->agent("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1");
-        	$ua->timeout(10);
+		$ua->timeout(30);
 
         	if (@proxies) {
         	        $tempProxy = selectRandomProxy();
@@ -267,7 +389,7 @@ package Proxy;
         	my $response = $ua->get($url);
         	if ($response->status_line !~ /200/) {
         	        removeProxy($tempProxy);
-			getURL2File($self,$url,$file);
+			getURL2File($self,$url,$file,$count+1);
 			return;
         	}
 
